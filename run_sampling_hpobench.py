038c5080914917cd8c8770e58a543719002932e5
Lukas Fehring <lukas.fehring@stud.uni-hannover.de>
2023-11-16 14:18:00 +0100

    Disable all run_types but smac. Only with for now. To be removed later

--- run_sampling_hpobench.py
+++ run_sampling_hpobench.py
@@ -26,7 +26,7 @@ class SurrogateModelCallback(Callback):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--job_id')
+    parser.add_argument('--job_id') #Todo What is this for?
     parser.add_argument('--run_type',
                         choices=["smac", "rand", "surr"],
                         help=
@@ -34,22 +34,31 @@ if __name__ == "__main__":
                         '"rand": Collect randomly sampled configurations and evaluate their performance,'
                         '"surr" Collect random samples, but estimated their performance using the Gaussian process '
                         '(Please note that, in the latter case, the BO sampling needs to be run beforehand to '
-                        'provide the Gaussian process models.)'
+                        'provide the Gaussian process models.)',
+                        default="smac"
+                        )    
+    parser.add_argument('--n_optimized_params',
+                        type=int,
+                        default=1,
+                        help='Number of hyperparameters to optimize. Defaults to 1.'
                         )
+    parser.add_argument('--n_seeds', type= int, default=5)
+    
     args = parser.parse_args()
     run_type = args.run_type
+    n_seeds = args.n_seeds
+    n_optimized_params = args.n_optimized_params
+    
+    assert run_type == "smac", "Only SMAC sampling is supported here. All other sampling methods are not maintained in this fork"
 
-    # number of HPs to optimize
-    n_optimized_params = 2
     # number of HP combinations to consider per model
-    max_hp_comb = 1
+    max_hp_comb = 1 #todo what does this mean
 
-    n_samples_spacing = np.linspace(20, 200, 10, dtype=int).tolist()
+    n_samples_spacing = [20]
     init_design_max_ratio = 0.25
     init_design_n_configs_per_hyperparamter = 8
 
     sampling_dir_name = "runs_sampling_hpobench"
-    n_seeds = 5
     surrogate_n_samples = 400
 
     run_conf = get_run_config(job_id=args.job_id, n_optimized_params=n_optimized_params, max_hp_comb=max_hp_comb)
@@ -65,20 +74,23 @@ if __name__ == "__main__":
         result_dict = b.objective_function(cfg, rng=seed)
         return result_dict['function_value']
 
-    if run_type == "rand":
-        n_samples_to_eval = [max(n_samples_spacing)]
+    # if run_type == "rand":
+    #     n_samples_to_eval = [max(n_samples_spacing)]
+    # else:
+    #     # SMAC uses at most scenario.n_trials * max_ratio number of configurations in the initial design
+    #     # If we run SMAC only once with n_trials = max(n_samples_spacing), we would always use the maximum number of
+    #     # initial designs, e.g. a run with 20 samples would have the same number of initial designs as a run with 200
+    #     # Thus, we do separate runs for each sample size as long as the number of initial designs would differ
+    #     if run_type == "surr":
+    #         n_samples_to_eval = n_samples_spacing
+    if run_type == "smac": # todo is this the same as run_type == "smac"?
+        # init design max ratio beschränkt die anzahl an hyperparmeter kombinationen die in der init design phase
+        n_samples_to_eval = [n for n in n_samples_spacing if
+                            init_design_max_ratio * n < n_optimized_params * init_design_n_configs_per_hyperparamter]#
+        if max(n_samples_spacing) not in n_samples_to_eval:
+            n_samples_to_eval.append(max(n_samples_spacing))
     else:
-        # SMAC uses at most scenario.n_trials * max_ratio number of configurations in the initial design
-        # If we run SMAC only once with n_trials = max(n_samples_spacing), we would always use the maximum number of
-        # initial designs, e.g. a run with 20 samples would have the same number of initial designs as a run with 200
-        # Thus, we do separate runs for each sample size as long as the number of initial designs would differ
-        if run_type == "surr":
-            n_samples_to_eval = n_samples_spacing
-        else:
-            n_samples_to_eval = [n for n in n_samples_spacing if
-                                 init_design_max_ratio * n < n_optimized_params * init_design_n_configs_per_hyperparamter]
-            if max(n_samples_spacing) not in n_samples_to_eval:
-                n_samples_to_eval.append(max(n_samples_spacing))
+        raise ValueError(f"Unknown or disabled run type {run_type}.")
 
     run_name = f"{model_name.replace(' ', '_')}_{'_'.join(optimized_parameters)}{data_set_postfix}"
 
@@ -118,28 +130,28 @@ if __name__ == "__main__":
 
             logger.info(f"Sample configs and train {model_name} with seed {seed}.")
 
-            if run_type == "rand":
-                configurations = cs.sample_configuration(size=n_samples)
-                performances = np.array(
-                    [b.objective_function(config.get_dictionary(), seed=seed)["function_value"] for config in
-                     configurations]
-                )
-                configurations = np.array(
-                    [list(i.get_dictionary().values()) for i in configurations]
-                ).T
-            elif run_type == "surr":
-                configurations = cs.sample_configuration(size=surrogate_n_samples)
-                configurations = np.array(
-                    [list(i.get_dictionary().values()) for i in configurations]
-                ).T
-                with open(f"results/{sampling_dir_name}/smac/{run_name}/surrogates/n_eval{n_eval}"
-                          f"_samples{n_samples}_seed{seed}.pkl", "rb") as surrogate_file:
-                    surrogate_model = pickle.load(surrogate_file)
-                performances = np.array(get_surrogate_predictions(configurations.T, cs, surrogate_model))
-            else:
+            # if run_type == "rand":
+            #     configurations = cs.sample_configuration(size=n_samples)
+            #     performances = np.array(
+            #         [b.objective_function(config.get_dictionary(), seed=seed)["function_value"] for config in
+            #          configurations]
+            #     )
+            #     configurations = np.array(
+            #         [list(i.get_dictionary().values()) for i in configurations]
+            #     ).T
+            # elif run_type == "surr":
+            #     configurations = cs.sample_configuration(size=surrogate_n_samples)
+            #     configurations = np.array(
+            #         [list(i.get_dictionary().values()) for i in configurations]
+            #     ).T
+            #     with open(f"results/{sampling_dir_name}/smac/{run_name}/surrogates/n_eval{n_eval}"
+            #               f"_samples{n_samples}_seed{seed}.pkl", "rb") as surrogate_file:
+            #         surrogate_model = pickle.load(surrogate_file)
+            #     performances = np.array(get_surrogate_predictions(configurations.T, cs, surrogate_model))
+            if run_type == "smac":
                 configurations, performances, _ = run_smac_optimization(
                     configspace=cs,
-                    facade=BlackBoxFacade,
+                    facade=BlackBoxFacade, # Stndard gaussian process no multi fidelity
                     target_function=optimization_function_wrapper,
                     function_name=model_name,
                     n_eval=n_samples,
@@ -149,6 +161,8 @@ if __name__ == "__main__":
                     max_ratio=init_design_max_ratio,
                     callback=SurrogateModelCallback()
                 )
+            else:
+                raise ValueError(f"Unknown or disabled run type {run_type}.")
 
             df = pd.DataFrame(
                 data=np.concatenate((configurations.T,
