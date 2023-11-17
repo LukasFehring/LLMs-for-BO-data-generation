7ab960a5327bbdee6182c667b839558457c1e6b2
Lukas Fehring <lukas.fehring@stud.uni-hannover.de>
2023-11-17 10:28:49 +0100

    Add jobid to slurm conf

--- run_sampling_hpobench.py
+++ run_sampling_hpobench.py
@@ -2,7 +2,6 @@ import os
 import numpy as np
 import pandas as pd
 import shutil
-import argparse
 import dill as pickle
 from smac import BlackBoxFacade, Callback
 from omegaconf import DictConfig
@@ -29,96 +28,96 @@ def main(cfg: DictConfig):
     n_samples = approach["n_samples"]
     
     n_seeds = approach["n_seeds"]
-    
+    job_id  = approach["job_id"]
     
     sampling_dir_name = "runs_sampling_hpobench"
     
     assert run_type == "smac", "Only SMAC sampling is supported here. All other sampling methods are not maintained in this fork"
 
     # number of HP combinations to consider per mode
-    for job_id in range(40):
-        run_conf = get_run_config(job_id=job_id, n_optimized_params=n_optimized_params, max_hp_comb=max_hp_comb)
+    
+    run_conf = get_run_config(job_id=job_id, n_optimized_params=n_optimized_params, max_hp_comb=max_hp_comb)
 
-        task_dict = get_task_dict()
-        data_set_postfix = f"_{task_dict[run_conf['task_id']]}"
-        optimized_parameters = list(run_conf["hp_conf"])
-        model_name = get_benchmark_dict()[run_conf["benchmark"]]
-        b = run_conf["benchmark"](task_id=run_conf["task_id"], hyperparameters=optimized_parameters)
+    task_dict = get_task_dict()
+    data_set_postfix = f"_{task_dict[run_conf['task_id']]}"
+    optimized_parameters = list(run_conf["hp_conf"])
+    model_name = get_benchmark_dict()[run_conf["benchmark"]]
+    b = run_conf["benchmark"](task_id=run_conf["task_id"], hyperparameters=optimized_parameters)
 
-        def optimization_function_wrapper(cfg, seed):
-            """ Helper-function: simple wrapper to use the benchmark with smac """
-            result_dict = b.objective_function(cfg, rng=seed)
-            return result_dict['function_value']
+    def optimization_function_wrapper(cfg, seed):
+        """ Helper-function: simple wrapper to use the benchmark with smac """
+        result_dict = b.objective_function(cfg, rng=seed)
+        return result_dict['function_value']
 
-        if run_type == "smac":
-            # init design max ratio beschränkt die anzahl an hyperparmeter kombinationen die in der init design phase
-            n_samples_to_eval = [n for n in n_samples_spacing if
-                                init_design_max_ratio * n < n_optimized_params * init_design_n_configs_per_hyperparamter]#
-            if max(n_samples_spacing) not in n_samples_to_eval:
-                n_samples_to_eval.append(max(n_samples_spacing))
-        else:
-            raise ValueError(f"Unknown or disabled run type {run_type}.")
+    if run_type == "smac":
+        # init design max ratio beschränkt die anzahl an hyperparmeter kombinationen die in der init design phase
+        n_samples_to_eval = [n for n in n_samples_spacing if
+                            init_design_max_ratio * n < n_optimized_params * init_design_n_configs_per_hyperparamter]#
+        if max(n_samples_spacing) not in n_samples_to_eval:
+            n_samples_to_eval.append(max(n_samples_spacing))
+    else:
+        raise ValueError(f"Unknown or disabled run type {run_type}.")
 
-        run_name = f"{model_name.replace(' ', '_')}_{'_'.join(optimized_parameters)}{data_set_postfix}"
+    run_name = f"{model_name.replace(' ', '_')}_{'_'.join(optimized_parameters)}{data_set_postfix}"
 
-        sampling_dir = f"results/{sampling_dir_name}/{run_type}"
-        sampling_run_dir = f"{sampling_dir}/{run_name}"
-        if os.path.exists(sampling_run_dir):
-            shutil.rmtree(sampling_run_dir)
-        os.makedirs(sampling_run_dir)
-        if run_type == "smac":
-            os.makedirs(f"{sampling_run_dir}/surrogates")
+    sampling_dir = f"results/{sampling_dir_name}/{run_type}"
+    sampling_run_dir = f"{sampling_dir}/{run_name}"
+    if os.path.exists(sampling_run_dir):
+        shutil.rmtree(sampling_run_dir)
+    os.makedirs(sampling_run_dir)
+    if run_type == "smac":
+        os.makedirs(f"{sampling_run_dir}/surrogates")
 
-        # setup logging
-        logger = get_logger(filename=f"{sampling_run_dir}/sampling_log.log")
+    # setup logging
+    logger = get_logger(filename=f"{sampling_run_dir}/sampling_log.log")
 
-        logger.info(f"Start {run_type} sampling for {run_name}.")
+    logger.info(f"Start {run_type} sampling for {run_name}.")
 
-        for acquisition_function in [LCB(), EI(), PI()]:
+    for acquisition_function in [LCB(), EI(), PI()]:
 
 
-            logger.info(f"Run: {run_name}")
-            logger.info(f"Start run to sample {n_samples} samples.")
+        logger.info(f"Run: {run_name}")
+        logger.info(f"Start run to sample {n_samples} samples.")
 
-            df_samples = pd.DataFrame()
+        df_samples = pd.DataFrame()
 
-            for i in range(n_seeds):
-                seed = i * 3
+        for i in range(n_seeds):
+            seed = i * 3
 
-                np.random.seed(seed)
+            np.random.seed(seed)
 
-                # add only parameters to be optimized to configspace
-                cs = b.get_configuration_space(seed=seed, hyperparameters=optimized_parameters)
+            # add only parameters to be optimized to configspace
+            cs = b.get_configuration_space(seed=seed, hyperparameters=optimized_parameters)
 
-                logger.info(f"Sample configs and train {model_name} with seed {seed}.")
+            logger.info(f"Sample configs and train {model_name} with seed {seed}.")
 
-                if run_type == "smac":
-                    configurations, performances, _ = run_smac_optimization(
-                        configspace=cs,
-                        facade=BlackBoxFacade, # Stndard gaussian process no multi fidelity
-                        acquisition_function=acquisition_function,
-                        target_function=optimization_function_wrapper,
-                        function_name=model_name,
-                        n_eval=n_samples,
-                        run_dir=sampling_run_dir,
-                        seed=seed,
-                        n_configs_per_hyperparamter=init_design_n_configs_per_hyperparamter,
-                        max_ratio=init_design_max_ratio,
-                    )
-                else:
-                    raise ValueError(f"Unknown or disabled run type {run_type}.")
+            if run_type == "smac":
+                configurations, performances, _ = run_smac_optimization(
+                    configspace=cs,
+                    facade=BlackBoxFacade, # Stndard gaussian process no multi fidelity
+                    acquisition_function=acquisition_function,
+                    target_function=optimization_function_wrapper,
+                    function_name=model_name,
+                    n_eval=n_samples,
+                    run_dir=sampling_run_dir,
+                    seed=seed,
+                    n_configs_per_hyperparamter=init_design_n_configs_per_hyperparamter,
+                    max_ratio=init_design_max_ratio,
+                )
+            else:
+                raise ValueError(f"Unknown or disabled run type {run_type}.")
 
-                df = pd.DataFrame(
-                    data=np.concatenate((configurations.T,
-                                        performances.reshape(-1, 1)), axis=1),
-                    columns=optimized_parameters + ["cost"])
-                df.insert(0, "seed", seed)
-                df = df.reset_index()
-                df = df.rename(columns={"index": "n_samples"})
-                df["n_samples"] = df["n_samples"] + 1
-                df_samples = pd.concat((df_samples, df))
+            df = pd.DataFrame(
+                data=np.concatenate((configurations.T,
+                                    performances.reshape(-1, 1)), axis=1),
+                columns=optimized_parameters + ["cost"])
+            df.insert(0, "seed", seed)
+            df = df.reset_index()
+            df = df.rename(columns={"index": "n_samples"})
+            df["n_samples"] = df["n_samples"] + 1
+            df_samples = pd.concat((df_samples, df))
 
-                df_samples.to_csv(f"{sampling_run_dir}/samples_{n_samples}.csv", index=False)
+            df_samples.to_csv(f"{sampling_run_dir}/samples_{n_samples}.csv", index=False)
 
 
     
